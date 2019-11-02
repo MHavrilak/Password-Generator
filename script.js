6196489025e49e8bf0029a6cbac82e9b94936668
Hava-hizzle <mikehavrilak@gmail.com>
2019-11-02 09:48:58 -0400

    Homework3

---
+++ script.js
@@ -0,0 +1,7 @@
+var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+="
+var lenght = document.getElementById.values;
+var password = ""
+
+for(var i=0; 1 <= length; i++ {
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)))
+})
