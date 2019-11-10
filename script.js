d8e606d24182efdab73cfc33604c266faab3edbf
Hava-hizzle <mikehavrilak@gmail.com>
2019-11-09 19:34:37 -0500

    revisions

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+="
+var valuesUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var valuesLow = "abcdefghijklmnopqrstuvwxyz"
+var valuesNum = "1234567890"
+var valuesSpec = "!@#$%^&*()_-+="
 var plength = ""
 var password = ""
+var valuesTot = ""
 
+// Generator Function
+function genpassword()
 
+// Initial Prompts
 var plength = parseInt(prompt( "Choose your password length."));
-    while (plength < 8 && plength > 128) {
+    while (plength < 8 || plength > 128) {
         alert("Please choose a different length.");
     }
 var valspec = prompt("Do you want special characters?");
@@ -14,11 +21,23 @@ var valnum = prompt("Do you want numbers?");
 
 if (valspec == null && valupp == null && vallow == null && valnum == null);
 
+if (valspec != null){
+    values = values + "!@#$%^&*()_-+="
+}else {
+    values = values + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}else {
+    values = values + "abcdefghijklmnopqrstuvwxyz"
+}else {
+    values = values + "1234567890"
+}
+
+// Random password funtion
 function values() {
 for(var i=0; 1 <= plength; i++) {
     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)))
 }
 }
+return password;
 
 //add password to textbox
 document.getElementById("password").values = password;
