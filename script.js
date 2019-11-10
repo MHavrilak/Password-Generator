c147281d710466a62426a209a1be385a61798e39
MHavrilak <55418489+MHavrilak@users.noreply.github.com>
2019-11-10 18:28:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
-var valuesUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var valuesLow = "abcdefghijklmnopqrstuvwxyz"
-var valuesNum = "1234567890"
-var valuesSpec = "!@#$%^&*()_-+="
-var plength = ""
-var password = ""
-var valuesTot = ""
+var valuesUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var valuesLow = "abcdefghijklmnopqrstuvwxyz";
+var valuesNum = "1234567890";
+var valuesSpec = "!@#$%^&*()_-+=";
+var plength = "";
+var password = "";
+var values = "";
+
+
+
+
 
 // Generator Function
-function genpassword()
+function genpassword(){
 
-// Initial Prompts
-var plength = parseInt(prompt( "Choose your password length."));
-    while (plength < 8 || plength > 128) {
-        alert("Please choose a different length.");
+    // Initial Prompts
+    plength = parseInt(prompt( "Choose your password length."));
+  
+    
+    if(plength < 8 || plength > 128){
+        alert('bad length')
+        genpassword();    
     }
-var valspec = prompt("Do you want special characters?");
-var valupp = prompt("Do you want UPPER CASE letters?");
-var vallow = prompt("Do you want lower case letters?");
-var valnum = prompt("Do you want numbers?");
 
-if (valspec == null && valupp == null && vallow == null && valnum == null);
+    var valspec = prompt("Do you want special characters?");
+    var valupp = prompt("Do you want UPPER CASE letters?");
+    var vallow = prompt("Do you want lower case letters?");
+    var valnum = prompt("Do you want numbers?");
 
-if (valspec != null){
-    values = values + "!@#$%^&*()_-+="
-}else {
-    values = values + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-}else {
-    values = values + "abcdefghijklmnopqrstuvwxyz"
-}else {
-    values = values + "1234567890"
+    if (valspec.toLowerCase() == 'y')
+        values = values + "!@#$%^&*()_-+=";
+        console.log(values);
+    if (valupp.toLowerCase() == 'y') 
+        values = values + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        console.log(values);
+    if (vallow.toLowerCase() == 'y')  
+        values = values + "abcdefghijklmnopqrstuvwxyz";
+        console.log(values);
+    if (valnum.toLowerCase() == 'y')
+        values = values + "1234567890";
+        console.log(values);
 }
+
 
 // Random password funtion
-function values() {
-for(var i=0; 1 <= plength; i++) {
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)))
+function getvalues() {
+    console.log(values.length);
+    console.log(values);
+    for(var i=0; i <= plength; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * (values.length -1)));
+        console.log(values.charAt(Math.floor(Math.random() * (values.length - 1))))
+    }
+    console.log(password);
+    document.getElementById("test_password").value = password;
 }
-}
-return password;
 
-//add password to textbox
-document.getElementById("password").values = password;
+
+
